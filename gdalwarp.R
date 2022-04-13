2ade6cde5948ab4644b0290e31c83a171fb32fa4
Jeronimo Rodriguez-Escobar <38987055+Cumaribo@users.noreply.github.com>
2022-04-13 18:48:12 -0400

    Update gdalwarp.R
    Some anonimization. just more general

--- gdalwarp.R
+++ gdalwarp.R
@@ -5,13 +5,14 @@ sapply(packs, require, character.only = TRUE)
 
 ##### r
 # imagen a alienar
-target<-'/storage/share/Angela_Share/DarienMask.tif'
- #este es mi mapa de referencia para alinear. pero en realidad puedes usar cualquier que esté alineaso
- reference. <-"/storage/home/TU/tug76452/Ecosistemas_Colombia/hansen_ideam/mask_ideam90_17.tif"
- #donde se va a poner: Share 
- dst.<-'/storage/share/Shared_summer_21/BioMapcol/DarienMask.tif'
+target<-'/path/target.tif'
+ #este es mi mapa de referencia para alinear. pero en realidad puedes usar cualquier que estÃ© alineaso
+ reference. <-"path/mask_ideam90_17.tif"
+ 
+#donde se va a poner: Share 
+ dst.<-'path/out/target.tif'
 #
-# correr función 
+# correr funciÃ³n 
  align_rasters(unaligned=target, reference=reference., dstfile=dst., nthreads=6, verbose= TRUE)
  
  
