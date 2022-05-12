826fb3c431a39e1abfbdbba59b1cae716dcfe941
Jeronimo Rodriguez-Escobar <38987055+Cumaribo@users.noreply.github.com>
2022-05-11 23:35:25 -0400

    Create data_man.R
    Start migrating to an 'utilities' repository.

---
+++ data_man.R
@@ -0,0 +1,3 @@
+I expect to create a portable toolbox of custom function at hand to perform analyisis  
+#This creates a vector pf zeros and length nrow de la tabla) But this is not what I need. Save it somewhere 
+repv <- list(unlist(replace_na(vector(mode='list', length =nrow(metrics_wide[[1]])),0)))   
