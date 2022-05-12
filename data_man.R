c8a3e2f260c745c235cfe0eaea0f9a13789201b2
Jeronimo Rodriguez-Escobar <38987055+Cumaribo@users.noreply.github.com>
2022-05-12 01:35:38 -0400

    Update data_man.R
    New fucntion. Recycled from preparing the cluster analysis

--- data_man.R
+++ data_man.R
@@ -1,3 +1,6 @@
 I expect to create a portable toolbox of custom function at hand to perform analyisis  
 #This creates a vector pf zeros and length nrow de la tabla) But this is not what I need. Save it somewhere 
 repv <- list(unlist(replace_na(vector(mode='list', length =nrow(metrics_wide[[1]])),0)))   
+
+#This replaces all NAs with zeros (not needed right now) 
+metrics_w <- map(metrics_w, . %>% replace(is.na(.),0)) 
